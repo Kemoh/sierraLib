0a33df3a582cf56c220e72f801c09e11bf18bb5e
Umaru Bayoh <bayohumau66@gmail.com>
2025-10-22 16:54:02 +0000

    made corrections

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/sierraLib/",
-
-  root: "./", // your index.html is in the project root
+  root: "src", // 👈 tells Vite your main HTML is in /src
+  base: "/sierraLib/", // 👈 must match your GitHub repo name
   build: {
-    outDir: "../dist", // folder for production build
+    outDir: "../dist", // 👈 build output one level up (in /dist)
+    emptyOutDir: true, // clean dist folder before new build
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
+        main: resolve(__dirname, "src/index.html"),
       },
     },
   },
