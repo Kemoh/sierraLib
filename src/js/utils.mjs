31b5fb61b5d2045248a90f3427817e1e1b97a026
Umaru Bayoh <bayohumau66@gmail.com>
2025-10-23 02:27:59 +0000

    Fix: include partials in build

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -68,8 +68,8 @@ export function renderWithTemplate(template, parentElement, data, callback) {
 export async function loadHeaderFooter() {
   try {
     // Absolute paths — Vite will rewrite them correctly
-    const headerTemplate = await loadTemplate("./partials/header.html");
-    const footerTemplate = await loadTemplate("./partials/footer.html");
+    const headerTemplate = await loadTemplate("partials/header.html");
+    const footerTemplate = await loadTemplate("partials/footer.html");
 
     const headerElement = document.querySelector("#main-header");
     const footerElement = document.querySelector("#main-footer");
